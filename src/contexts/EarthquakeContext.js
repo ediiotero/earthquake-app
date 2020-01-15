5eab41422080b6d25acc51bc0c6a5582365b5308
Eddie <e.otero81@gmail.com>
2020-01-15 18:22:30 -0500

    working on getting map img

--- src/contexts/EarthquakeContext.js
+++ src/contexts/EarthquakeContext.js
@@ -11,7 +11,6 @@ const EarthquakeContextProvider = props => {
     "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson";
 
   const getEarthquakeMap = place => {
-    setEarthquakeMap([]);
     const mapKey = process.env.REACT_APP_MAPQUEST_KEY;
     const url = `https://open.mapquestapi.com/staticmap/v5/map?key=${mapKey}&center=${place}&size=@2x`;
     fetch(url)
